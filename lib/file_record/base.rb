d1b27a63468cdeaab9e67f82bd814d155a2e4f71
Michał Taszycki <mtaszycki@gmail.com>
2011-02-10 01:29:55 +0100

    conversion

--- lib/file_record/base.rb
+++ lib/file_record/base.rb
@@ -1,6 +1,23 @@
 module FileRecord
   class Base
 
+    # include ActiveModel::Conversion
+    def to_model
+      self
+    end
+
+    def persisted?
+      false
+    end
+
+    def to_key
+      persisted? ? [id] : nil
+    end
+
+    def to_param
+      persisted? ? to_key.join('-') : false
+    end
+
   end
   
 end
