9069798032c0b7fe0db8918bcfedea76aff0d356
Michał Taszycki <mtaszycki@gmail.com>
2011-03-01 19:19:38 +0100

    form controller fixes

--- test/dummy/app/controllers/addresses_controller.rb
+++ test/dummy/app/controllers/addresses_controller.rb
@@ -1,34 +1,34 @@
 class AddressesController < ApplicationController
   # GET /addresses
-  # GET /addresses.xml
+  # GET /addresses.json
   def index
     @addresses = Address.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @addresses }
+      format.json  { render :json => @addresses }
     end
   end
 
   # GET /addresses/1
-  # GET /addresses/1.xml
+  # GET /addresses/1.json
   def show
     @address = Address.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @address }
+      format.json  { render :json => @address }
     end
   end
 
   # GET /addresses/new
-  # GET /addresses/new.xml
+  # GET /addresses/new.json
   def new
     @address = Address.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @address }
+      format.json  { render :json => @address }
     end
   end
 
@@ -38,46 +38,46 @@ class AddressesController < ApplicationController
   end
 
   # POST /addresses
-  # POST /addresses.xml
+  # POST /addresses.json
   def create
     @address = Address.new(params[:address])
 
     respond_to do |format|
       if @address.save
         format.html { redirect_to(@address, :notice => 'Address was successfully created.') }
-        format.xml  { render :xml => @address, :status => :created, :location => @address }
+        format.json  { render :json => @address, :status => :created, :location => @address }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @address.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @address.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /addresses/1
-  # PUT /addresses/1.xml
+  # PUT /addresses/1.json
   def update
     @address = Address.find(params[:id])
 
     respond_to do |format|
       if @address.update_attributes(params[:address])
         format.html { redirect_to(@address, :notice => 'Address was successfully updated.') }
-        format.xml  { head :ok }
+        format.json  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @address.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @address.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /addresses/1
-  # DELETE /addresses/1.xml
+  # DELETE /addresses/1.json
   def destroy
     @address = Address.find(params[:id])
     @address.destroy
 
     respond_to do |format|
       format.html { redirect_to(addresses_url) }
-      format.xml  { head :ok }
+      format.json  { head :ok }
     end
   end
 end
